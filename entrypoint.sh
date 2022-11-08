cf2a530e7d99c193b7e9bf2da8015e3de28bfd53
martinformi <formanko@develmagic.com>
2022-11-08 23:40:12 +0100

    feat: adding output to github env

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,16 +10,19 @@ readonly fail_on_diff="$5"
 if [ "$breaking_only" = "true" ] && [ "$fail_on_diff" = "true"  ]
 then
   echo "running breaking_only and fail_on_diff..."
-  oasdiff -breaking-only -fail-on-diff -format "$format" -base "$base" -revision "$revision"
+  output=$(oasdiff -breaking-only -fail-on-diff -format "$format" -base "$base" -revision "$revision")
 elif [ "$breaking_only" = "true" ]
 then
   echo "running breaking_only..."
-  oasdiff -breaking-only -format "$format" -base "$base" -revision "$revision"
+  output=$(oasdiff -breaking-only -format "$format" -base "$base" -revision "$revision")
 elif [ "$fail_on_diff" = "true" ]
 then
   echo "running fail_on_diff..."
-  oasdiff -fail-on-diff -format "$format" -base "$base" -revision "$revision"
+  output=$(oasdiff -fail-on-diff -format "$format" -base "$base" -revision "$revision")
 else
   echo "running default..."
-  oasdiff -format "$format" -base "$base" -revision "$revision"
+  output=$(oasdiff -format "$format" -base "$base" -revision "$revision")
 fi
+
+echo $oasdiff_output
+echo "oasdiff_output=$output" >> $GITHUB_ENV
